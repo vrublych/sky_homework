ac75535ecdc495af2602987d43d930f3128d2a27
vrublych <vrublych34@gmail.com>
2023-12-11 22:41:04 +0300

    add new funcs

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def word_upper(string):
+    """
+    принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return string.upper()
+
+def my_function(string):
+    """
+    принимает на вход строку,
+    которая делает заглавными первые буквы
+    каждого слова в строке
+    """
+    result = ""
+    for word in string.split():
+        result += word.capitalize() + " "
+    return result
